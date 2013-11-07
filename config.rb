f5b9fc81e0c1b9b8d979cbccdf2a8f315e2a8427
pkarl <pete.karl@gmail.com>
2013-11-07 13:32:20 -0500

    set up workspace

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "src/sass"
+images_dir = "img"
+javascripts_dir = "src/js"
+fonts_dir = "src/fonts"
+relative_assets = true
+add_import_path "components/upbase/components"
+line_comments = false
+output_style = :compact
