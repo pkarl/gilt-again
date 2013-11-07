a69569f1ea6006f1694e6a17efc8a842817cdc75
pkarl <pete.karl@gmail.com>
2013-11-07 14:44:20 -0500

    updated Tech in README

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,8 @@ module.exports = function(grunt) {
 	// load all grunt tasks
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-react');
+	grunt.loadNpmTasks('grunt-browserify');
 
 	// set up config
 	grunt.initConfig({
@@ -14,26 +16,44 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 
 		compass: {
-			dist: {
-				// options: {
-				// 	config: 'config.rb'
-				// }
-			}
+			dist: {}
 		},
 
 		watch: {
 			scripts: {
-				files: ['src/app/style/sass/*.scss'],
-				tasks: ['default'],
+				files: ['src/app/style/sass/*.scss','src/app/templates/jsx/*.jsx'],
+				tasks: ['compass', 'react'],
 				options: {
-					debounceDelay: 1000,
+					debounceDelay: 500,
 					nonull: true
+				}
+			}
+		},
+
+		react: {
+			app: {
+				options: {
+					extension: 'js',
+					ignoreMTime: false
+				},
+				files: {
+					'src/app/js': 'src/app/templates/jsx'
 				}
 			},
 		},
 
+		browserify: {
+			options: {
+				transform: [require('grunt-react').browserify]
+			},
+			app: {
+				src: 'src/app/js/main.js',
+				dest: 'src/app/js/built.js'
+			}
+		}
+
 	});
 
-	grunt.registerTask('default', ['compass', 'watch']);
+	grunt.registerTask('default', ['compass', 'react', 'watch']);
 
 };
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-gilt-again
-==========
+# Gilt (again)
 
 A re-creation of Gilt's web site using Backbone &amp; React for learning purposes.
+
+## Tech
+
+* [Grunt](http://gruntjs.com/)
+* [Bower](http://bower.io/)
+* [SCSS](http://sass-lang.com/) ([compass](http://compass-style.org/))
+* [Backbone.js](http://backbonejs.org/)
+* [React/JSX](http://facebook.github.io/react/docs/getting-started.html)
+* [Browserify](http://browserify.org/)
+* [Mocha](https://github.com/visionmedia/mocha)
+* [Chai](http://chaijs.com/)
+* [Sinon](http://sinonjs.org/)
